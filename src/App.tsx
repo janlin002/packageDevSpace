5281d4149b4de9fbd70e0ea706dcf6f7c368c869
janlin002 <123davidbill@gmail.com>
2023-06-20 10:46:39 +0800

    adjust

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,24 @@
 import React from 'react'
 
-// import Test from './test'
+import MuiTreeSelect from './MuiTreeSelect'
 
-function App() {
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
 
+import data from './fakeData.json'
+
+const Test = () => {
   return (
-    // <MuiTreeSelect 
-    //   treeData={data}
-    //   collapseIcon={<ExpandMoreIcon />}
-    //   expandIcon={<ChevronRightIcon/>}
-    //   iconReverse={true}
-    //   // checkboxColor={'red'}
-    //   // expanded={['1']}
-    // />
-    // <Test />
-    <div>123</div>
+    <MuiTreeSelect 
+      treeData={data}
+      collapseIcon={<ExpandMoreIcon />}
+      expandIcon={<ChevronRightIcon/>}
+      iconReverse={true}
+      // checkboxColor={'red'}
+      // expanded={['1']}
+    />
   )
 }
 
-export default App
+export default Test
+
--- src/index.tsx
+++ src/index.tsx
@@ -1,2 +1,9 @@
-import MuiTreeSelect from './MuiTreeSelect'
-export default MuiTreeSelect
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
